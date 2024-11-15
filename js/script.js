7ce58dcf86db1d56c746b253a592efe6dc76caf2
RowanTheAccussed <126895389+RowanTheAccussed@users.noreply.github.com>
2024-11-15 15:21:26 +0000

    fgff

--- js/script.js
+++ js/script.js
@@ -139,7 +139,7 @@ const tempChart = new Chart("feelsLike", {
   }
   });
 
-  // setInterval(fetchData, 500);
+setInterval(fetchData, 500);
 
 
 
@@ -208,64 +208,3 @@ const tempChart = new Chart("feelsLike", {
 
 
 
-
-
-
-
-// const humiditySlider = document.getElementById("humidity-slider");
-// const windSlider = document.getElementById("wind-slider");
-// const temperatureSlider = document.getElementById("temperature-slider");
-
-// const temperatureValue = document.getElementById("temperature-value");
-// const humidityValue = document.getElementById("humidity-value");
-// const windValue = document.getElementById("wind-value");
-
-// const leafAnimations = document.getElementsByClassName("leaf");
-
-// const humidityFilter = document.getElementById("humidity-filter");
-
-// async function fetchData() {
-//   try {
-
-//     const response = await fetch(apiUrl);
-
-//     if (!response.ok) {
-//       throw new Error("Response Status: ", response.status);
-//     }
-
-//     const json = await response.json();
-
-//     udpateHumidity(json.current.humidity);
-//     updateWind(json.current.wind_speed);
-//     updateTemperature(json.current.temperature);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function udpateHumidity(newValue) {
-  
-//   humiditySlider.value = newValue;
-//   humidityValue.innerHTML = newValue;
-
-//   humidityFilter.style.opacity = (0.5 * Number(newValue)) / 100;
-// }
-
-// function updateWind(newValue) {
-//   windSlider.value = newValue;
-//   windValue.innerHTML = newValue;
-
-//   let newDuration = ((408 - Number(newValue)) * 11) / 408 + 1;
-//   if(newDuration < 0.1) newDuration = 0.1;
-
-//   for (const leaf of leafAnimations) {
-//     leaf.style.animationDuration = newDuration + "s";
-//   }
-// }
-
-// function updateTemperature(newValue) {
-//   temperatureSlider.value = newValue;
-//   temperatureValue.innerHTML = newValue;
-// }
-
-// setInterval(fetchData, 500);
